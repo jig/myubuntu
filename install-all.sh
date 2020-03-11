9c0aada432fca54d8d775b6f38b711ad9b7109fe
Adria Alcaraz <Adria.Alcaraz@entrustdatacard.com>
2020-03-11 14:41:53 +0100

    testing execution of script

--- install-all.sh
+++ install-all.sh
@@ -29,7 +29,7 @@ everything() {
   # sudo sed -i "s@/es.@/us.@" /etc/apt/sources.list
 
   # Install begins
-  BRANCH="master"
+  BRANCH="feature/go1.14"
   bash -c "$(wget https://raw.githubusercontent.com/jig/myubuntu/$BRANCH/install-distroupdate.sh -O -)"
   
   bash -c "$(wget https://raw.githubusercontent.com/jig/myubuntu/$BRANCH/install-basepackages.sh -O -)"
