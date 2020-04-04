13799b0e1e7867132be7aecaef6b6767805a3872
Jordi Íñigo <jordi.inigo@entrustdatacard.com>
2020-04-04 11:44:00 +0200

    Go v1.14

--- install-golang.sh
+++ install-golang.sh
@@ -31,7 +31,7 @@ golang() {
   ###################################
   # Golang
 
-  VERSION=1.14
+  VERSION=1.14.1
 
   printf "${BLUE}Installing Go...${NORMAL}\n"
   wget https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz -O /tmp/golang.tgz
