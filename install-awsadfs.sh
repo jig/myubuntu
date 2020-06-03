2589350c43ee81dcb56f5571a0795bfcb98fdba9
Marc Marti Calabres <marc.marti.calabres@gmail.com>
2020-06-03 17:00:43 +0200

    feat: Improved Deployment Scripts
    * Support for Vault HA
    * Handling of consul-template via HELM
    * Private CA supported for Vault Connection
    * Removed CLUSTER_TAG and changed to CLUSTER_NAME
    * Removed AWS_PROFILE in favor of aws-adfs
    
    Related to PKIAL-395

---
+++ install-awsadfs.sh
@@ -0,0 +1,33 @@
+install_awsadfs() {
+  # Use colors, but only if connected to a terminal, and that terminal
+  # supports them.
+  if which tput >/dev/null 2>&1; then
+      ncolors=$(tput colors)
+  fi
+  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+  else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+  fi
+
+  set -e
+
+  printf "${BLUE}Installing aws-adfs...${NORMAL}\n"
+
+  apt-get -y install python3.6 python3-pip libkrb5-dev
+  pip3 install aws-adfs
+
+  aws-adfs --version
+}
+
+install_awsadfs
