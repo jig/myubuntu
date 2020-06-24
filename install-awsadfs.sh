bd82ed3b856ec5db1fda6728b072703e0ebfd735
Jordi Íñigo <jig@users.noreply.github.com>
2020-06-24 21:48:07 +0200

    Corrected AWS ADFS not installing

--- install-awsadfs.sh
+++ install-awsadfs.sh
@@ -24,7 +24,7 @@ install_awsadfs() {
 
   printf "${BLUE}Installing aws-adfs...${NORMAL}\n"
 
-  apt-get -y install python3.6 python3-pip libkrb5-dev
+  sudo apt-get -y install python3.6 python3-pip libkrb5-dev
   pip3 install -U pyopenssl
   pip3 install aws-adfs
 
