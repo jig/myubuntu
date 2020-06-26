c819a5579d4f070a86d9a131801a959941477d5a
Jordi Íñigo <jig@users.noreply.github.com>
2020-06-26 20:21:50 +0200

    Update install-kubernetes.sh

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -53,8 +53,8 @@ install_kops() {
   # kind
   # This requires go and docker
   printf "${BLUE}Installing kind (kubernetes in docker)...${NORMAL}\n"
-  curl -Lo $(go env GOPATH)/bin/kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
-  chmod +x $(go env GOPATH)/bin/kind
+  curl -Lo ~/go/bin/kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
+  chmod +x ~/go/bin/kind
   # Create docker network for KIND
   sudo docker network create --driver=bridge --subnet=172.18.0.0/16 --ip-range=172.18.0.0/24 --gateway=172.18.0.1 kind || true
 }
