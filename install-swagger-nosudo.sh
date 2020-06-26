96edfa04754e95f1d99f85e5df9383a6a661a571
Jordi Íñigo <jig@users.noreply.github.com>
2020-06-26 21:24:20 +0200

    Jig (#36)
    * Create install-swagger-nosudo.sh
    
    * Update install-swagger-nosudo.sh
    
    * Update install-swagger.sh

---
+++ install-swagger-nosudo.sh
@@ -0,0 +1,42 @@
+install_swagger_nosudo() {
+  # Use colors, but only if connected to a terminal, and that terminal
+  # supports them.
+  if which tput >/dev/null 2>&1; then
+      ncolors=$(tput colors)
+  fi
+  if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+  else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+  fi
+
+  set -e
+
+  printf "${BLUE}Installing Pandoc...${NORMAL}\n"
+
+  apt-get update 
+  apt install jq pandoc -y
+
+  printf "${BLUE}Installing go-swagger...${NORMAL}\n"
+
+  VERSION=v0.21.0
+
+  export download_url=$(curl -s https://api.github.com/repos/go-swagger/go-swagger/releases/tags/${VERSION} | jq -r '.assets[] | select(.name | contains("'"$(uname | tr '[:upper:]' '[:lower:]')"'_amd64")) | .browser_download_url')
+  curl -o /tmp/swagger -L'#' "$download_url"
+  mv /tmp/swagger /usr/local/bin/swagger
+  chmod +x /usr/local/bin/swagger 
+
+  swagger version
+}
+
+install_swagger_nosudo
